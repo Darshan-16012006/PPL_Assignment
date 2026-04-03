2258983845050984794baf069e224a50b7d0ef37
Darshan-16012006 <laddarshan6@gmail.com>
2026-04-03 18:42:53 +0530

    Add files via upload

---
+++ LabAssignment/Practical8/Assignment2.py
@@ -0,0 +1,14 @@
+src = input("Enter source file name: ")
+dest = input("Enter destination file name: ")
+
+f1 = open(src,"r")
+f2 = open(dest,"w")
+
+for line in f1:
+    if not line.strip().startswith("#"):
+        f2.write(line)
+
+f1.close()
+f2.close()
+
+print("File copied without comments.")
